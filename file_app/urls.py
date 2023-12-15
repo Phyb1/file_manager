5aaaf630daf4fb84845bfc34de1690519c87c4c2
Phibeon <phibeon@yahoo.com>
2023-12-15 14:59:39 +0200

    adding file upload and download issue the view function upload_file doesn;t redirect to file list after a successul save

--- file_app/urls.py
+++ file_app/urls.py
@@ -3,4 +3,6 @@ from .import views
 app_name ='file_app'
 urlpatterns = [
     path('', views.file_list, name='file_list'),
+    path('upload/', views.upload_file, name='upload'),
+    path('download/<int:file_id>/', views.download_file, name='download'),
 ]
--- file_app/views.py
+++ file_app/views.py
@@ -10,4 +10,20 @@ def file_list(request):
     return render(request, 'file_list.html', {'files':files})
 
 
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('file_list')
+    else:
+        form = UploadFileForm()
+        return render(request, 'upload_file.html', {'form':form})
+        
+
+def download_file(request, file_id):
+    uploaded_file =UploadFile.objects.get(pk=file_id)
+    response = FileResponse(uploaded_file.file, as_attachment=True)
+    return response
+
 
