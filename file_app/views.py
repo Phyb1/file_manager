4168d4c2e114cf2ce564865258d9a10c40c8b159
Phybeon <85971263+Phyb1@users.noreply.github.com>
2024-05-11 23:13:12 -0700

    Update views.py

--- file_app/views.py
+++ file_app/views.py
@@ -1,29 +1,57 @@
-from django.shortcuts import render, redirect,get_object_or_404
+from django.db.models import F
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, View
 from django.http import FileResponse
+from django.core.paginator import Paginator
 from .forms import UploadFileForm
-from .models import  UploadFile
+from .models import UploadFile
 
-# Create your views here.
+class FileListView(ListView):
+    model = UploadFile
+    template_name = 'file_list.html'
+    context_object_name = 'files'
+    paginate_by = 10  # Number of files per page
+    ordering = ['upload_date']  # Default ordering (by upload date)
 
-def file_list(request):
-    files =UploadFile.objects.all()
-    return render(request, 'file_list.html', {'files':files})
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        sort_by = self.request.GET.get('sort_by')
+        if sort_by == 'date':
+            queryset = queryset.order_by('-upload_date')  # Sort by upload date (descending)
+        elif sort_by == 'file_type':
+            queryset = queryset.order_by('file')  # Sort by file type
+        elif sort_by == 'size':
+            queryset = queryset.annotate(file_size=F('file__size')).order_by('file_size')  # Sort by file size
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        paginator = Paginator(context['files'], self.paginate_by)
+        page_number = self.request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        context['page_obj'] = page_obj
+        return context
 
 
-def upload_file(request):
-    if request.method == 'POST':
+class FileDownloadView(View):
+    def get(self, request, file_id):
+        uploaded_file = get_object_or_404(UploadFile, pk=file_id)
+        try:
+            response = FileResponse(uploaded_file.file, as_attachment=True)
+            return response
+        except FileNotFoundError:
+            return render(request, 'error.html', {'message': 'File not found.'})
+        except Exception as e:
+            return render(request, 'error.html', {'message': f'An error occurred: {str(e)}'})
+
+class FileUploadView(View):
+    def get(self, request):
+        form = UploadFileForm()
+        return render(request, 'upload_file.html', {'form': form})
+
+    def post(self, request):
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('file_app:file_list')
-    else:
-        form = UploadFileForm()
-        return render(request, 'upload_file.html', {'form':form})
-        
-
-def download_file(request, file_id):
-    uploaded_file = get_object_or_404(UploadFile, pk=file_id)
-    response = FileResponse(uploaded_file.file, as_attachment=True)
-    return response
-
-
+        return render(request, 'upload_file.html', {'form': form})
