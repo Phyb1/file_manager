74fdab23355535e5bdcef05d84cb6cfe2b1457c5
Phybeon <85971263+Phyb1@users.noreply.github.com>
2024-05-11 22:19:45 -0700

    Update forms.py
    Setting the title to file name

--- file_app/forms.py
+++ file_app/forms.py
@@ -2,7 +2,11 @@ from django import forms
 from .models import UploadFile
 
 class UploadFileForm(forms.ModelForm):
-    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].widget = forms.TextInput(attrs={'readonly': True})  # Set title field as read-only
+        self.fields['title'].help_text = "Automatically set to the filename."
+
     class Meta:
         model = UploadFile
         fields = ("title", 'file',)
