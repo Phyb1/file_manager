de7b5f0d054dc7bb49453325a1499de53afd4f50
Phybeon <85971263+Phyb1@users.noreply.github.com>
2024-05-11 23:59:27 -0700

    Update models.py

--- file_app/models.py
+++ file_app/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
-# Create your models here.
 class UploadFile(models.Model):
-    title = models.CharField(max_length=100)
     file = models.FileField(upload_to='uploads/')
     upload_date = models.DateField(auto_now_add=True)
 
+    def save(self, *args, **kwargs):
+        if not self.title:
+            self.title = self.file.name.split('/')[-1]  # Extract file name from file path
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.title
-
