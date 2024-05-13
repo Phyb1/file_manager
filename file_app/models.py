2b42c6b8bb2bd2906d447d76cd02254ef18cb3fa
Phibeon <phibeon@yahoo.com>
2024-05-13 08:13:00 -0700

    enabled title field to be blank

--- file_app/models.py
+++ file_app/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # Create your models here.
 class UploadFile(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100 ,blank=True)
     file = models.FileField(upload_to='uploads/')
     upload_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+    
 
